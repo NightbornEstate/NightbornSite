dc549bf4813056ad28d4607c7a618e27991a8929
Jay Williams <course@jaywilliams.me>
2017-10-29 18:51:04 +0000

    Updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
-
+app.use(express.static("assets"))
 
 app.get('/', function (req, res) {
   res.render("pages/index.ejs")
