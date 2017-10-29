4b47c5023a029e1f08a711e4c781ce62aafdde3d
Jay Williams <course@jaywilliams.me>
2017-10-29 18:07:43 +0000

    Whoo a ton of stuff yhyh

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+
+
+app.get('/', function (req, res) {
+  res.render("pages/index.ejs")
+})
+app.get('/staff', function (req, res) {
+  res.render("pages/staff.ejs")
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
