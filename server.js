fc48ab7eec2e8a7280f8569fd5eb7144278e1a63
Jay Williams <course@jaywilliams.me>
2017-11-03 19:55:06 +0000

    Port 80

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static("assets"))
 
-app.get('/', function (req, res) {
-  res.render("pages/index.ejs")
+app.get('/', function(req, res) {
+    res.render("pages/index.ejs")
 })
-app.get('/staff', function (req, res) {
-  res.render("pages/staff.ejs")
+app.get('/staff', function(req, res) {
+    res.render("pages/staff.ejs")
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function() {
+    console.log('Example app listening on port 3000!')
 })
