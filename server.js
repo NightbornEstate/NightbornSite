ac40bc787acb4cadf987815324324f2b2cfebcd5
Jay Williams <codingJWilliams@users.noreply.github.com>
2019-10-14 14:41:09 +0100

    Add comments to server.js

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static("assets"))
-
+// Home page
 app.get('/', function(req, res) {
     res.render("pages/index.ejs")
 })
+// Staff Page
 app.get('/staff', function(req, res) {
     res.render("pages/staff.ejs")
 })
-
+// Listen
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
 })
